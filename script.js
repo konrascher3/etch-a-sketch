c0b969f248b23446a9403c14a9d0cb523940954b
Konrad Scherb <Konrad.Scher@gmx.de>
2021-10-12 15:55:30 +0200

    Reformat grid

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const drawArea = document.querySelector('.draw-area');
 console.log(drawArea);
+// create 16x16 grid
 for (let i = 0; i < 256; i++) {
     const pixel = document.createElement('div');
     pixel.classList.add(`pixel${i}`)
-    pixel.textContent = `thisIsPixel${i}`
+    pixel.textContent = `${i}`
     drawArea.appendChild(pixel)
 }
