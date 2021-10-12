fdf45e54249486b9789aa543a494d3765b6b64a2
Konrad Scherb <Konrad.Scher@gmx.de>
2021-10-12 16:06:37 +0200

    add drawBlack function

--- script.js
+++ script.js
@@ -4,6 +4,19 @@ console.log(drawArea);
 for (let i = 0; i < 256; i++) {
     const pixel = document.createElement('div');
     pixel.classList.add(`pixel${i}`)
+    pixel.setAttribute('id', 'pixel')
     pixel.textContent = `${i}`
     drawArea.appendChild(pixel)
+}
+
+const pixels = document.querySelectorAll('#pixel');
+pixels.forEach((pixel) => {
+    pixel.addEventListener('mouseover', (event) => {
+        console.log(event.target.textContent)
+        drawBlack(event.target)
+    })
+})
+
+function drawBlack(event) {
+    event.style.backgroundColor = 'black'
 }
